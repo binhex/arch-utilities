60e54c761ebb7f838925c8adcfbd15d61041c0cf
binhex <megalith01@gmail.com>
2020-07-15 12:05:14 +0100

    rework start script

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
+# create day variable from todays date and time
+datetime=$(date +%Y%m%d-%H%M%S)
+
+rclone_config="/config/rclone/config/rclone.conf"
+rclone_log="/config/rclone/logs/rclone_${datetime}.log"
+
 # create folder structure for config, temp and logs
-mkdir -p /config/urbackup/sqlite/tmp
-mkdir -p /config/urbackup/log
-mkdir -p /config/urbackup/tmp
-mkdir -p /config/urbackup/config
+mkdir -p /config/rclone/config /config/rclone/logs
 
-if [[ ! -f /config/urbackup/config/urbackupsrv ]]; then
+if [ ! -f "${rclone_config}" ]; then
 
-	# copy default config to volume map
-	cp /etc/default/urbackupsrv /config/urbackup/config/urbackupsrv
+	echo "[warn] rclone config file does not exist"
 
-	# set default location for backup storage to /media
-	echo "/media" > /var/urbackup/backupfolder
+else
+
+	# run rclone
+	/usr/bin/rclone --config="${rclone_config}" copy /media/Pictures encrypt:/Pictures --log-file="${rclone_log}" --log-level INFO
 
 fi
-
-# run urbackup server
-/usr/bin/urbackupsrv run --config /config/urbackup/config/urbackupsrv --no-consoletime
